5ac4d4ae660c51910152a4ebb38106b47cb1e5a9
Sumbati10 <lindashirasala@gmail.com>
2025-04-03 13:57:27 +0300

    updated the experince section

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,134 +1,168 @@
 export const Bio = {
-    name: "Sumbati Lindah",
-    roles: [
-      "Data Scientist",
-      "Data Analyst",
-      "UI/UX Designer",
-      "Programmer",
+  name: "Sumbati Lindah",
+  roles: [
+    "Data Scientist",
+    "Data Analyst",
+    "ML Engineer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Sumbati10",
+  resume: "https://drive.google.com/file/d/1VgcV8Z0E3bQX4O_gkZqZ03iWZ0DqNGrO/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/lindahsumbati/"
+};
+
+export const skills = [
+  {
+    title: "Machine Learning",
+    skills: [
+      {
+        name: "TensorFlow",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/a2/TensorFlow_logo.svg",
+      },
+      {
+        name: "Scikit Learn",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/800px-Scikit_learn_logo_small.svg.png",
+      },
+      {
+        name: "Keras",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Keras_logo.png/800px-Keras_logo.png",
+      },
+      {
+        name: "PyTorch",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/PyTorch_logo.png/800px-PyTorch_logo.png",
+      },
+      {
+        name: "XGBoost",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/3c/XGBoost_logo.png",
+      },
+      {
+        name: "LightGBM",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/LightGBM_logo.png/600px-LightGBM_logo.png",
+      },
     ],
-    description:
-      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/Sumbati10",
-    resume: "https://drive.google.com/file/d/1VgcV8Z0E3bQX4O_gkZqZ03iWZ0DqNGrO/view?usp=sharing",
-    linkedin:"https://www.linkedin.com/in/lindahsumbati/"
-   };
-   export const skills = [
-    {
-      title: "Machine Learning",
-      skills: [
-        {
-          name: "TensorFlow",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/a/a2/TensorFlow_logo.svg",
-        },
-        {
-          name: "Scikit Learn",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/800px-Scikit_learn_logo_small.svg.png",
-        },
-        {
-          name: "Keras",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Keras_logo.png/800px-Keras_logo.png",
-        },
-        {
-          name: "PyTorch",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/PyTorch_logo.png/800px-PyTorch_logo.png",
-        },
-        {
-          name: "XGBoost",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/3/3c/XGBoost_logo.png",
-        },
-        {
-          name: "LightGBM",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/LightGBM_logo.png/600px-LightGBM_logo.png",
-        },
-      ],
-    },
-  ];
-  export const experiences = [
-    {
-      id: 0,
-      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
-      role: "Data Scientist",
-      company: "Hello Tractor",
-      date: "January 2024 - Present",
-      desc: `
-        • Selected features, building and optimizing classifiers using machine learning techniques.
-        • Extended company’s data with third party sources of information when needed from the database.
-        • Enhanced data collection procedures to include information that is relevant for building analytic systems.
-        • Implemented natural language processing (NLP) techniques to analyse user-generated text data for Agriculture App.
-        • Designed and deployed cloud-based data pipelines for real-time analytics.
-        • Conducted predictive analytics to optimize resource allocation and improve operational efficiency in financial data analysis.
-        • Collaborated with international teams to provide insights on agricultural development projects.
-        • Conducted remote sensing and GIS to extract data using Google Earth Engine.
-        • Processed, cleansed, and verified the integrity of data used for analysis.
-        • Conducted ad-hoc analysis and A/B testing to evaluate the effectiveness of new business strategies.
-      `,
-      skills: [
-        "Machine Learning",
-        "NLP",
-        "Data Pipelines",
-        "Google Earth Engine",
-        "Data Processing",
-        "Predictive Analytics",
-        "Cloud Computing",
-      ],
-    },
-    {
-      id: 1,
-      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
-      role: "Data Analyst",
-      company: "Kenya Agricultural and Livestock Research Organization",
-      date: "2018 - 2020",
-      desc: `
-        • Developed and implemented innovative data collection systems and strategies, optimizing statistical efficiency and elevating data quality; reduced data processing time by 40% and increased data accuracy by 25%.
-        • Applied expertise in ETL/ELT principles to optimize data processing and management, contributing to improved data quality and efficiency.
-        • Built Power BI dashboards and reports that are easily understandable by stakeholders, enhancing the accessibility and usability of data-driven insights, ultimately improving the decision-making process.
-        • Developed and maintained dashboards that tracked KPIs, facilitating a 25% increase in overall organizational performance.
-        • Contributed to project and methodology design such as analytical frameworks, resulting in more robust research methodologies and project structures that increased project efficiency by 30%.
-        • Acquired data from secondary and primary data sources, maintained databases and systems, ensuring data integrity and accessibility, resulting in a 25% reduction in data retrieval time.
-      `,
-      skills: [
-        "Power BI",
-        "ETL/ELT",
-        "Data Collection",
-        "SQL",
-        "Data Integrity",
-        "Dashboard Development",
-        "Data Analytics",
-      ],
-    },
-    {
-      id: 2,
-      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
-      role: "Data Analyst Intern",
-      company: "KPMG AU Data Analytics",
-      date: "2022",
-      desc: `
-        • Applied strong proficiency in data analysis tools such as SQL and Python to meet project requirements and stakeholder expectations.
-        • Ensured accuracy and relevance in reporting, fostering a culture of data-driven decision-making.
-        • Engaged with clients, investors, and development finance institutions (DFIs) to present data-driven insights and recommendations.
-        • Supported external users, including debt and equity investors, by providing clear and actionable data insights tailored to their needs.
-        • Demonstrated analytical thinking and problem-solving skills, contributing to the success of projects in a dynamic and challenging environment.
-        • Utilized strong proficiency in data visualization tools such as PowerBI to create visually compelling representations and dashboards of complex data.
-        • Enhanced stakeholder understanding by delivering clear and actionable insights through effective data visualization.
-        • Assessed data quality and completeness in preparation for analysis, conducting a thorough evaluation of data sources to ensure accuracy and reliability, laying the foundation for robust and credible analysis.
-        • Presented clear and concise data insights to diverse stakeholders, utilizing compelling visuals and storytelling techniques.
-      `,
-      skills: [
-        "SQL",
-        "Python",
-        "Power BI",
-        "Data Visualization",
-        "Data Analytics",
-        "Stakeholder Engagement",
-      ],
-    },
-  ];
+  },
+  {
+    title: "Technical Skills",
+    skills: [
+      {
+        category: "Programming Languages",
+        skills: [
+          "Python (NumPy, Pandas, Scikit-learn)",
+          "R Language",
+          "STATA",
+        ],
+      },
+      {
+        category: "Data Visualization",
+        skills: [
+          "Microsoft PowerBI",
+          "Matplotlib",
+          "Seaborn",
+          "LookML (Looker)",
+        ],
+      },
+      {
+        category: "Statistical Analysis",
+        skills: [
+          "Hypothesis Testing",
+          "Time Series Analysis",
+          "Regression Analysis",
+          "A/B Testing",
+        ],
+      },
+      {
+        category: "Database Management",
+        skills: [
+          "MYSQL",
+          "PostgreSQL",
+        ],
+      },
+      {
+        category: "Data Science",
+        skills: [
+          "Predictive Modeling",
+          "Natural Language Processing (NLP)",
+          "Statistical Analysis",
+          "Data Visualization",
+          "Machine Learning",
+          "Deep Learning",
+          "Artificial Intelligence (AI)",
+        ],
+      },
+      {
+        category: "Tools",
+        skills: [
+          "Excel",
+          "Google Analytics",
+          "Jupyter Notebook",
+          "Git",
+          "Google Sheets",
+          "GitHub",
+        ],
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    role: "Data Scientist",
+    company: "Hello Tractor",
+    date: "January 2024 - Present",
+    desc: `
+      • Selected features, building and optimizing classifiers using machine learning techniques.
+      • Extended company’s data with third party sources of information when needed from the database.
+      • Enhanced data collection procedures to include information that is relevant for building analytic systems.
+      • Implemented natural language processing (NLP) techniques to analyse user-generated text data for Agriculture App.
+      • Designed and deployed cloud-based data pipelines for real-time analytics.
+      • Conducted predictive analytics to optimize resource allocation and improve operational efficiency in financial data analysis.
+      • Collaborated with international teams to provide insights on agricultural development projects.
+      • Conducted remote sensing and GIS to extract data using Google Earth Engine.
+      • Processed, cleansed, and verified the integrity of data used for analysis.
+      • Conducted ad-hoc analysis and A/B testing to evaluate the effectiveness of new business strategies.
+    `,
+    skills: [
+      "Machine Learning",
+      "NLP",
+      "Data Pipelines",
+      "Google Earth Engine",
+      "Data Processing",
+      "Predictive Analytics",
+      "Cloud Computing",
+    ],
+  },
+  {
+    id: 1,
+    role: "Data Analyst",
+    company: "Kenya Agricultural and Livestock Research Organization",
+    date: "2018 - 2020",
+    desc: `
+      • Developed and implemented innovative data collection systems and strategies, optimizing statistical efficiency and elevating data quality; reduced data processing time by 40% and increased data accuracy by 25%.
+      • Applied expertise in ETL/ELT principles to optimize data processing and management, contributing to improved data quality and efficiency.
+      • Built Power BI dashboards and reports that are easily understandable by stakeholders, enhancing the accessibility and usability of data-driven insights, ultimately improving the decision-making process.
+      • Developed and maintained dashboards that tracked KPIs, facilitating a 25% increase in overall organizational performance.
+      • Contributed to project and methodology design such as analytical frameworks, resulting in more robust research methodologies and project structures that increased project efficiency by 30%.
+      • Acquired data from secondary and primary data sources, maintained databases and systems, ensuring data integrity and accessibility, resulting in a 25% reduction in data retrieval time.
+    `,
+    skills: [
+      "Power BI",
+      "ETL/ELT",
+      "Data Collection",
+      "SQL",
+      "Data Processing",
+      "Database Management",
+    ],
+  },
+];
+
   
   export const education = [
     {
